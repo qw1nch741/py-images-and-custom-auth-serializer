2192c72ca17ef88bdec726258b8684578e0e9cd6
Kyryl <sautakiki.com@gmail.com>
2026-04-30 19:06:31 +0300

    sol

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,19 @@
-from django.core.exceptions import ValidationError
-from django.db import models
+import os
+import uuid
+
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import (
+    migrations,
+    models
+)
+from django.utils.text import slugify
+
+
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/movies/", filename)
 
 
 class CinemaHall(models.Model):
@@ -41,7 +54,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to="uploads/")
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     class Meta:
         ordering = ["title"]
